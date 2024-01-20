a8892cd8ed6ecdbc64a1fa555cba26e41835b6ed
reececorkhill <reece@smartit-webdevelopment.com>
2024-01-20 21:30:45 +0000

    Added global variables.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,48 @@
 $( document ).ready(function() {
     console.log( "ready!" );
+
+    // Global Variables
+    var today = dayjs();
+    var currentDate = document.getElementById("#currentDay"); // Getting the currentDay element.
+    var timeblocksCtn = document.getElementsByClassName(".container"); // Getting the timeblocks container.
+    var workingDay = [
+        {
+            hour: "9AM", // [0]
+            task: "", // [1]
+        },
+        {
+            hour: "10AM", // [0]
+            task: "", // [1]
+        },
+        {
+            hour: "11AM", // [0]
+            task: "", // [1]
+        },
+        {
+            hour: "12PM", // [0]
+            task: "", // [1]
+        },
+        {
+            hour: "1PM", // [0]
+            task: "", // [1]
+        },
+        {
+            hour: "2PM", // [0]
+            task: "", // [1]
+        },
+        {
+            hour: "3PM", // [0]
+            task: "", // [1]
+        },
+        {
+            hour: "4PM", // [0]
+            task: "", // [1]
+        },
+        {
+            hour: "5PM", // [0]
+            task: "", // [1]
+        },
+    ];
+
+    console.log(today);
 });
